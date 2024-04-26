ac296becd85cf74b40ab45d083951c6aa13ef6d8
Omkar <opawar449@gmail.com>
2024-04-26 13:12:08 +0530

    update the code

--- src/main/java/RSA/pageobjects/CartPage.java
+++ src/main/java/RSA/pageobjects/CartPage.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
-
 import RSA.Abstract.AbstractCompoenent;
 
 public class CartPage extends AbstractCompoenent {
